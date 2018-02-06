429b21894dc493a17db26818f88e156448c80323
Steven Harper <shwesh279@gmail.com>
2018-02-06 12:17:25 -0800

    added through command line

--- rxthing_for_ultrasonicsensor-neo/rxthing_for_ultrasonicsensor-neo.ino
+++ rxthing_for_ultrasonicsensor-neo/rxthing_for_ultrasonicsensor-neo.ino
@@ -20,38 +20,33 @@ void setup() {
 
 void loop() {
         int inches;
-
+        String inchstr;
 
         
         // send data only when you receive data:
         if (Serial.available() > 0) {
-                // read the incoming byte:
-                incomingByte = Serial.read();
-
-                // say what you got:
-                Serial.print("I received: ");
-                Serial.println(incomingByte);
-               
+              
       int condition=1; //conditionis int type
       if(condition==1){
       int first =Serial.read(); //check for the start of a message, noted by "R"
+      
       if (first=="R"){
       condition=0;
       //recieve the digits that follow the "R"
       Range[0]=Serial.read();
       Range[1]=Serial.read();
-      Range[2]=Serial.read();
+     
       int i;
-      String inchstr;
+      
       //iterate through Range and concatenate the digits together
-         for ( i = 0; i < 3; i++){
-      if(Range[i] != "R" && Range[i] != NULL){
+         for ( i = 0; i < 2; i++){
+      if(Range[i] != 82 && Range[i] != NULL){
         //add the digits gotten by concatenation (ex. 2+2 = 22)
-        inchstr = inchstr + Range[i];
-        
+        String S = Range[i]-48;
+        inchstr = inchstr + Range;
+        Serial.println(Range[i]);
       }
       }
-      //convert the string to an integer
       inches = inchstr.toInt();
       Serial.println(inches);
       set_neopixel(inches);
@@ -66,4 +61,3 @@ void set_neopixel(int inches){
     strip.setPixelColor(i, strip.Color(0,0,0,255 ));
   }
 }
-
